5991dcdd97362ac2727fc1ce1a47db8ca5962d0a
Bloemenpot <mjssmith03@gmail.com>
2022-12-07 00:23:37 +0100

    Day 1 and Day 6 Done :+1:

--- Day 1/Advent2022Day1/Advent2022Day1/Program.cs
+++ Day 1/Advent2022Day1/Advent2022Day1/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Advent2022Day1
+﻿using System.IO;
+
+namespace Advent2022Day1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> ints = new List<int>();
+            int currentElf = 0;
+            int currentCalories = 0;
+
+            foreach (string line in System.IO.File.ReadAllLines(@"input.txt"))
+            {
+                //Console.WriteLine(line);
+                if (line.Length > 0)
+                {
+                    //Console.WriteLine("geen lege regel");
+                    currentCalories += int.Parse(line);
+                } else
+                {
+                    ints.Insert(currentElf, currentCalories);
+                    currentElf++;
+                    currentCalories = 0;
+                }
+            }
+
+            ints.Insert(currentElf, currentCalories);
+            currentElf++;
+            currentCalories = 0;
+
+            Console.WriteLine(ints.Count.ToString());
+
+            ints.Sort();
+
+            //for (int i = 0; i < ints.Count; i++)
+            //{
+
+            //    //Console.WriteLine("Elf {0} has: {1} calories", i, ints[i]);
+            //}
+
+            int nr1 = ints[ints.Count - 1];
+            int nr2 = ints[ints.Count - 2];
+            int nr3 = ints[ints.Count - 3];
+            int total = nr1 + nr2 + nr3;
+
+            Console.WriteLine("Top 3:");
+            Console.WriteLine("#1: {0}", nr1);
+            Console.WriteLine("#2: {0}", nr2);
+            Console.WriteLine("#3: {0}", nr3);
+            Console.WriteLine("Total of top 3: {0}", total);
         }
     }
 }
---
+++ Day 6/Advent2022Day6/Advent2022Day6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Advent2022Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"input.txt");
+
+            char[] chars = text.ToCharArray();
+
+            List<char> charList = new List<char>();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                charList.Add(chars[i]);
+                if (i > 12)
+                {
+                    try
+                    {
+                        string full = "";
+                        int amountWrong = 0;
+                        foreach (char c in charList)
+                        {
+
+                            if (!full.Contains(c))
+                            {
+                                amountWrong++;
+                                if (amountWrong == 14)
+                                {
+                                    Console.WriteLine("REEE 14 NIET DEZELFDE {0}", i + 1);
+                                    return;
+                                }
+                            }
+                            full += c;
+                        }
+                        amountWrong= 0;
+                        full = "";
+
+                        charList.Remove(charList.First());
+                    } catch { }
+                }
+            }
+        }
+    }
+}
