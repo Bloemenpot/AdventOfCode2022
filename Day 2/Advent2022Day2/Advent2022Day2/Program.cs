ed7a2340fbf409f2317e9589052ae0ecbf35d803
Bloemenpot <mjssmith03@gmail.com>
2022-12-07 01:09:09 +0100

    Done with part 1 of day 2. Stuck on part 2 for now

---
+++ Day 2/Advent2022Day2/Advent2022Day2/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Advent2022Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 0;
+            Dictionary<int, string> map = new Dictionary<int, string>();
+            map.Add(1, "A");
+            map.Add(2, "B");
+            map.Add(3, "C");
+            map.Add(1, "X");
+            map.Add(2, "Y");
+            map.Add(3, "Z");
+
+            foreach (string line in System.IO.File.ReadAllLines(@"example.txt"))
+            {
+                string[] current = line.Split(" ");
+                switch (current[1])
+                {
+                    case "X":
+                        points += 1;
+                        break;
+                    case "Y":
+                        points += 2;
+                        break;
+                    case "Z":
+                        points += 3;
+                        break;
+                    default: break;
+                }
+
+                switch (current[0])
+                {
+                    case "A":
+                        if (current[1] == "X")
+                        {
+
+                        }
+                        if (current[1] == "Y")
+                        {
+
+                        }
+                        if (current[1] == "Z")
+                        {
+
+                        }
+                        break;
+                    case "B":
+                        if (current[1] == "X") { points += 0; }
+                        if (current[1] == "Y") { points += 3; }
+                        if (current[1] == "Z") { points += 6; }
+                        break;
+                    case "C":
+                        if (current[1] == "X") { points += 6; }
+                        if (current[1] == "Y") { points += 0; }
+                        if (current[1] == "Z") { points += 3; }
+                        break;
+                    default: break;
+                }
+            }
+            Console.WriteLine(points);
+        }
+    }
+}
